26bb1bef8b56c72705787058abf7e3550702a47c
TrueBuyers <147467891+TrueBuyers@users.noreply.github.com>
2023-10-11 00:51:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     var generateButton = document.getElementById('generateButton');
+    var generateButtonForWebhook = document.getElementById('generateButtonForWebhook');
     generateButton.addEventListener('click', generateDetails);
+    generateButtonForWebhook.addEventListener('click', generateDetailsForWebhook);
 });
 
 function generateDetails() {
     var name = document.getElementById('name').value;
     var number = document.getElementById('number').value;
-    var adress = document.getElementById('adress').value;
+    var propertyType = document.getElementById('property_type').value;
     var bedrooms = document.getElementById('bedrooms').value;
     var bathrooms = document.getElementById('bathrooms').value;
     var garage = document.getElementById('garage').value;
@@ -34,12 +36,10 @@ function generateDetails() {
     var redfinLink = document.getElementById('redfin_link').value;
     var caller = document.getElementById('caller').value;
 
-    var webhookUrl = document.getElementById('webhook').value; // Get the webhook URL
-
     var formattedMessage = `
     Name: ${name}
     Phone Number: ${number}
-    Adress: ${adress}
+    Property Type: ${propertyType}
     Bedrooms: ${bedrooms}
     Bathrooms: ${bathrooms}
     Garage or Storage Space: ${garage}
@@ -68,17 +68,85 @@ function generateDetails() {
     Caller: ${caller}
     `;
 
-    // Discord webhook URL
-    var discordWebhookUrl = webhookUrl; // Use the webhook URL from the form
+    var webhookUrl = document.getElementById('webhook').value;
+    sendToWebhook(webhookUrl, formattedMessage);
+}
 
-    // Send data to Discord channel via webhook
-    fetch(discordWebhookUrl, {
+function generateDetailsForWebhook() {
+    var name = document.getElementById('name').value;
+    var number = document.getElementById('number').value;
+    var propertyType = document.getElementById('property_type').value;
+    var bedrooms = document.getElementById('bedrooms').value;
+    var bathrooms = document.getElementById('bathrooms').value;
+    var garage = document.getElementById('garage').value;
+    var renovations = document.getElementById('renovations').value;
+    var damage = document.getElementById('damage').value;
+    var sellingReason = document.getElementById('selling_reason').value;
+    var mortgages = document.getElementById('mortgages').value;
+    var isRented = document.getElementById('is_rented').checked ? 'Yes' : 'No';
+    var askingPrice = document.getElementById('asking_price').value;
+    var propertyRating = document.getElementById('property_rating').value;
+    var zestimate = document.getElementById('zestimate').value;
+    var isListed = document.getElementById('is_listed').checked ? 'Yes' : 'No';
+    var garageCars = document.getElementById('garage_cars').value;
+    var leaseType = document.getElementById('lease_type').value;
+    var leaseExpire = document.getElementById('lease_expire').value;
+    var callBackTime = document.getElementById('call_back_time').value;
+    var size = document.getElementById('size').value;
+    var condition = document.getElementById('condition').value;
+    var occupancy = document.getElementById('occupancy').value;
+    var motivation = document.getElementById('motivation').value;
+    var timeframe = document.getElementById('timeframe').value;
+    var motivationLevel = document.getElementById('motivation_level').value;
+    var zillowLink = document.getElementById('zillow_link').value;
+    var redfinValue = document.getElementById('redfin_value').value;
+    var redfinLink = document.getElementById('redfin_link').value;
+    var caller = document.getElementById('caller').value;
+
+    var formattedMessage = `
+    Name: ${name}
+    Phone Number: ${number}
+    Property Type: ${propertyType}
+    Bedrooms: ${bedrooms}
+    Bathrooms: ${bathrooms}
+    Garage or Storage Space: ${garage}
+    Recent Renovations or Upgrades: ${renovations}
+    Damage that Needs Repair: ${damage}
+    Reason for Selling: ${sellingReason}
+    Existing Mortgages or Liens: ${mortgages}
+    Is the Property Rented? ${isRented}
+    Asking Price: ${askingPrice}
+    Property Rating: ${propertyRating}
+    Zestimate: ${zestimate}
+    Is the Property Listed? ${isListed}
+    Number of Garage Cars: ${garageCars}
+    Lease Type: ${leaseType}
+    Lease Expiry Date: ${leaseExpire}
+    Best Time to Call Back: ${callBackTime}
+    Property Size (sq. ft.): ${size}
+    Condition: ${condition}
+    Occupancy: ${occupancy}
+    Motivation: ${motivation}
+    Timeframe: ${timeframe}
+    Motivation Level: ${motivationLevel}
+    Zillow Link: ${zillowLink}
+    Redfin Value: ${redfinValue}
+    Redfin Link: ${redfinLink}
+    Caller: ${caller}
+    `;
+
+    var webhookUrl = 'https://discord.com/api/webhooks/1161080803887951943/jbH6VWTJjROTsRIW-8nWR0mSyGORlKbiCliCrmM01mHS3k5Y4gD1fQbE4lNkRoPsJQe-';
+    sendToWebhook(webhookUrl, formattedMessage);
+}
+
+function sendToWebhook(url, message) {
+    fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            content: formattedMessage,
+            content: message,
         }),
     })
     .then(response => {
@@ -88,11 +156,9 @@ function generateDetails() {
         return response.text();
     })
     .then(data => {
-        console.log(data); // Log the response from Discord webhook (optional)
+        console.log(data);
     })
     .catch(error => {
         console.error('There has been a problem with your fetch operation:', error);
     });
-
-
 }
